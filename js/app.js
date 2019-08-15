db8e39be6bb3f70f220fd335b0ba8f1dab921d3c
Meder Kamalov <meder@kamalov.net>
2019-08-15 12:51:34 +0200

    Process registration

--- js/app.js
+++ js/app.js
@@ -8,15 +8,41 @@ const registration = new Vue({
     student: false,
     member: false,
     party: false,
+    file: '',
   },
 
   methods: {
     async sendData() {
       if (!this.fname || !this.lname) message.notify('Please enter your name', 'error');
       else {
-        // Send data to server
+        let formdata = new FormData();
+        formdata.set('fname', this.fname);
+        formdata.set('lname', this.lname);
+        formdata.set('affil', this.affil);
+        formdata.set('student', this.student);
+        formdata.set('member', this.member);
+        formdata.set('party', this.party);
+        
+        try {
+          this.file = this.$refs.file.files[0];
+        } catch (e) {
+          console.log(e);
+        }
+        
+        if (this.file) formdata.append('file', this.file);
+        message.notify('Processing', 'warning');
+        let res = await axios({
+          url: 'http://localhost:3000/items',
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          data: formdata,
+        }).catch(err => message.notify('There was a problem, please try again later', 'error'));
+        message.toggle();
+        if (res) message.notify('Your registration information successfully sent to conference organizers. You will recieve a confirmation in the coming days.');
       }
-    }
+    },
   },
 });
 
@@ -24,7 +50,8 @@ const message = new Vue({
   el: '#vue-message',
 
   data: {
-    success: '',
+    status: '',
+    message: '',
     show: false,
   },
 
@@ -36,7 +63,8 @@ const message = new Vue({
     notify(message, status) {
       console.log(message);
       this.show = true;
-      this[status] = message;
+      this.message = message;
+      this.status = status;
     },
   }
 });
